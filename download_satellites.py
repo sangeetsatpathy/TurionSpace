6f1374785f627b1177aff4750fa19cf79f69e5df
Sangeet Satpathy <59673667+sangeetsatpathy@users.noreply.github.com>
2025-06-17 10:12:36 -0700

    Delete download_satellites.py

--- download_satellites.py
+++
@@ -1,8 +0,0 @@
-from skyfield.api import load
-
-max_days = 7.0         # download again once 7 days old
-name = 'noaa_stations.tle'  # custom filename, not 'gp.php'
-
-url = 'https://www.celestrak.org/NORAD/elements/gp.php?GROUP=noaa&FORMAT=tle'
-
-load.download(url, filename=name)
